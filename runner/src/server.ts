9add3c883cea2918dc64e4bed37703a503be76c0
needhamcookn <needhamcookn@gmail.com>
2026-08-06 12:10:09 -0700

    Add single-process and zygote container flags for Railway Chromium

--- runner/src/server.ts
+++ runner/src/server.ts
@@ -45,6 +45,10 @@ class SessionManager {
             "--no-sandbox",
             "--disable-setuid-sandbox",
             "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--single-process",
             "--disable-gpu",
           ],
         });
